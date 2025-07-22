0d8ef7ba2ef6d09877b468c442048e7a5fe4c1ee
joshua <udiajoshua@gmail.com>
2025-07-22 14:09:27 -0800

    second

--- pyqt_learning/app.py
+++ pyqt_learning/app.py
@@ -1,20 +1,33 @@
 """A normal application interface"""
-from PyQt5.QtWidgets import QApplication, QWidget
+# from PyQt5.QtWidgets import QApplication, QWidget
+# from PyQt5.QtCore import *
+# from PyQt5.QtGui import *
+
+# # Only needed for access to command line arguments
+# import sys
+
+# # receive command-line args form terminal
+# # and pass them into the application
+# app = QApplication(sys.argv)
+
+# # this will create the window of the QApplication
+# window = QWidget() 
+
+# # default, the window is always hidden so, you show
+# window.show()
+
+# # start the event loop
+# app.exec()
+
+
+
+"""This proves that you still will have a window without QWidget"""
+import sys 
+from PyQt5.QtWidgets import QApplication, QPushButton
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 
-# Only needed for access to command line arguments
-import sys
-
-# receive command-line args form terminal
-# and pass them into the application
 app = QApplication(sys.argv)
-
-# this will create the window of the QApplication
-window = QWidget() 
-
-# default, the window is always hidden so, you show
+window = QPushButton("Push Me")
 window.show()
-
-# start the event loop
 app.exec()
