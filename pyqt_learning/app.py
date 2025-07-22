97074816b6b42e0477e1e2cdf1d1ce4f028428f7
joshua <udiajoshua@gmail.com>
2025-07-22 15:18:47 -0800

    Forth

--- pyqt_learning/app.py
+++ pyqt_learning/app.py
@@ -1,4 +1,4 @@
-"""A normal application interface"""
+"""A normal application interface 1"""
 # from PyQt5.QtWidgets import QApplication, QWidget
 # from PyQt5.QtCore import *
 # from PyQt5.QtGui import *
@@ -21,7 +21,7 @@
 
 
 
-"""This proves that you still will have a window without QWidget"""
+"""This proves that you still will have a window without QWidget 2"""
 # import sys 
 # from PyQt5.QtWidgets import QApplication, QPushButton
 # from PyQt5.QtCore import *
@@ -33,7 +33,7 @@
 # app.exec()
 
 
-"""Doing the same thing but with a class"""
+"""Doing the same thing but with a class 3"""
 # from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
 # from PyQt5.QtGui import *
 # from PyQt5.QtCore import *
@@ -58,25 +58,51 @@
 
 
 
-"""This futher explains the"""
-import sys 
-from PyQt5.QtCore import QSize, Qt
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
+"""This futher explains the 4"""
+# import sys 
+# from PyQt5.QtCore import QSize, Qt
+# from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
 
-# Subclass QMainWindow to customize your application's moin window
+# # Subclass QMainWindow to customize your application's moin window
+# class MainWindow(QMainWindow):
+#     def __init__(self):
+#         super().__init__()
+        
+#         self.setWindowTitle("My App")
+
+#         button = QPushButton("Press Me!")
+#         # Set the central widget of the window.
+#         self.setCentralWidget(button)
+        
+
+
+# app= QApplication(sys.argv)  # This enables us to Event Loop
+# window= MainWindow()  # THis, will take the Moin Object of the sub class of the MainWindow
+# window.show()
+# app.exec()
+
+
+
+"""This shows us 5"""
+import sys 
+from PyQt5.QtCore import QSize, Qt  # this is on the configuration a bit...
+import PyQt5.QtGui  # this is on the Graphics
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        
+
         self.setWindowTitle("My App")
+        # it sets the size of the window
+        self.setFixedSize(QSize(500,200))
+        # self.setMinimumSize(QSize(565,565))
+        # self.setMaximumSize(QSize(565,565))
 
         button = QPushButton("Press Me!")
-        # Set the central widget of the window.
         self.setCentralWidget(button)
         
-
-
-app= QApplication(sys.argv)  # This enables us to Event Loop
-window= MainWindow()  # THis, will take the Moin Object of the sub class of the MainWindow
+        
+app = QApplication(sys.argv)
+window= MainWindow()
 window.show()
 app.exec()
